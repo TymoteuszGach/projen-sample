a98d83142aaa20be9ffe6c5b0bf466b95a7bab57
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-09-21 22:31:40 +0200

    chore: disable license

--- .projenrc.ts
+++ .projenrc.ts
@@ -12,6 +12,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
       printWidth: 120,
     },
   },
+  licensed: false,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
