fb936ef27d65719b810a64ad036dbb7c129b22e1
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2022-09-21 22:29:42 +0200

    chore: enable prettier

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,17 @@
-import { awscdk, javascript } from 'projen';
+import { awscdk, javascript } from "projen";
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'projen-sample',
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  name: "projen-sample",
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
-  gitignore: ['.idea'],
+  gitignore: [".idea"],
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      printWidth: 120,
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
